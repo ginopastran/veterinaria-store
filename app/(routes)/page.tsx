e7e3804f0b7d0dbdb38307c9982d4652fc06b338
ginopastran <ginopastran@gmail.com>
2024-01-04 20:48:25 -0300

    Deletes

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -11,6 +11,7 @@ const HomePage = async () => {
 
   return (
     <Container>
+      <Billboard />
       <div className="space-y-10 pb-10">
         {/* <Billboard data={billboard} /> */}
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
